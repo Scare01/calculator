6c984c5472c53d000639542534aaf7aef56e48a7
Aleksei Slesarev <lexsl84@gmail.com>
2018-10-01 18:42:28 +0700

    ongoing

--- src/index.js
+++ src/index.js
@@ -9,29 +9,34 @@ class App extends React.Component {
             display: 0,
             output: ''
         }
+        this.clickButton = this.clickButton.bind(this);
     }
+
+    clickButton() {}
+
     render() {
         return (<div id="calc">
             <div id="display">0</div>
             <div id="buttons">
-                <div className="button" id="clear">C</div>
-                <div className="button" id="divide">/</div>
-                <div className="button" id="multiply">*</div>
-                <div className="button" id="seven">7</div>
-                <div className="button" id="eight">8</div>
-                <div className="button" id="nine">9</div>
-                <div className="button" id="subtruct">-</div>
-                <div className="button" id="four">4</div>
-                <div className="button" id="five">5</div>
-                <div className="button" id="six">6</div>
-                <div className="button" id="add">+</div>
-                <div className="button" id="one">1</div>
-                <div className="button" id="two">2</div>
-                <div className="button" id="three">3</div>
-                <div className="button" id="equal">=</div>
-                <div className="button" id="zero">0</div>
-                <div className="button" id="decimal">.</div>
+                <div className="button" style={this.state.styleButton} onClick={this.clickButton} id="clear">C</div>
+                <div className="button" style={this.state.styleButton} id="divide">/</div>
+                <div className="button" style={this.state.styleButton} id="multiply">*</div>
+                <div className="button" style={this.state.styleButton} id="seven">7</div>
+                <div className="button" style={this.state.styleButton} id="eight">8</div>
+                <div className="button" style={this.state.styleButton} id="nine">9</div>
+                <div className="button" style={this.state.styleButton} id="subtruct">-</div>
+                <div className="button" style={this.state.styleButton} id="four">4</div>
+                <div className="button" style={this.state.styleButton} id="five">5</div>
+                <div className="button" style={this.state.styleButton} id="six">6</div>
+                <div className="button" style={this.state.styleButton} id="add">+</div>
+                <div className="button" style={this.state.styleButton} id="one">1</div>
+                <div className="button" style={this.state.styleButton} id="two">2</div>
+                <div className="button" style={this.state.styleButton} id="three">3</div>
+                <div className="button" style={this.state.styleButton} id="equal">=</div>
+                <div className="button" style={this.state.styleButton} id="zero">0</div>
+                <div className="button" style={this.state.styleButton} id="decimal">.</div>
             </div>
+            <div id="copyright">RubyLupus</div>
         </div>);
     }
 }
