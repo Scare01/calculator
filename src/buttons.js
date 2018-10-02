9658f334c6a65fa25236611350bbb6dadd9db425
Aleksei Slesarev <lexsl84@gmail.com>
2018-10-02 14:38:57 +0700

    some change

---
+++ src/buttons.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class Buttons extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div id="buttons">
+            <button className="button" id="clear" onClick={this.props.initialize}>C</button>
+            <button className="button" id="buttonide">/</button>
+            <button className="button" id="multiply">*</button>
+            <button className="button" id="seven">7</button>
+            <button className="button" id="eight">8</button>
+            <button className="button" id="nine">9</button>
+            <button className="button" id="subtract">-</button>
+            <button className="button" id="four">4</button>
+            <button className="button" id="five">5</button>
+            <button className="button" id="six">6</button>
+            <button className="button" id="add">+</button>
+            <button className="button" id="one">1</button>
+            <button className="button" id="two">2</button>
+            <button className="button" id="three">3</button>
+            <button className="button" id="equal">=</button>
+            <button className="button" id="zero">0</button>
+            <button className="button" id="decimal">.</button>
+        </div>)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Buttons from './buttons.js'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            display: 0,
-            output: ''
+            currentVal: '0',
+            prevVal: '0',
+            formule: '',
+            lastClicked: ''
         }
-        this.clickButton = this.clickButton.bind(this);
+        this.initialize = this.initialize.bind(this);
     }
 
-    clickButton() {}
+    initialize() {
+        this.setState({currentVal: '0', prevVal: '0', formule: '', lastClicked: ''});
+    }
 
     render() {
         return (<div id="calc">
-            <div id="display">0</div>
-            <div id="buttons">
-                <div className="button" style={this.state.styleButton} onClick={this.clickButton} id="clear">C</div>
-                <div className="button" style={this.state.styleButton} id="divide">/</div>
-                <div className="button" style={this.state.styleButton} id="multiply">*</div>
-                <div className="button" style={this.state.styleButton} id="seven">7</div>
-                <div className="button" style={this.state.styleButton} id="eight">8</div>
-                <div className="button" style={this.state.styleButton} id="nine">9</div>
-                <div className="button" style={this.state.styleButton} id="subtruct">-</div>
-                <div className="button" style={this.state.styleButton} id="four">4</div>
-                <div className="button" style={this.state.styleButton} id="five">5</div>
-                <div className="button" style={this.state.styleButton} id="six">6</div>
-                <div className="button" style={this.state.styleButton} id="add">+</div>
-                <div className="button" style={this.state.styleButton} id="one">1</div>
-                <div className="button" style={this.state.styleButton} id="two">2</div>
-                <div className="button" style={this.state.styleButton} id="three">3</div>
-                <div className="button" style={this.state.styleButton} id="equal">=</div>
-                <div className="button" style={this.state.styleButton} id="zero">0</div>
-                <div className="button" style={this.state.styleButton} id="decimal">.</div>
-            </div>
+            <div id="display">{this.state.display}</div>
+            <Buttons initialize={this.initialize}/>
             <div id="copyright">RubyLupus</div>
         </div>);
     }
